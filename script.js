ac6ad716e70f9e27e0c7e30472166dca12eb51d8
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-04-10 16:30:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,6 @@ class Rectangle {
     this._width = width;
     this._height = height;
   }
-
-  get width() {
-    return this._width;
-  }
-
-  get height() {
-    return this._height;
-  }
-
   getArea() {
     return this._width * this._height;
   }
